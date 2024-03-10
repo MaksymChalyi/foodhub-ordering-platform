1a6d2c12fa563f316fc22987b82a7f971704fd15
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-03-11 00:04:47 +0200

    init project

---
+++ src/main/java/com/maksimkaxxl/foodhuborderingplatform/FoodHubOrderingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.maksimkaxxl.foodhuborderingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodHubOrderingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodHubOrderingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/maksimkaxxl/foodhuborderingplatform/FoodHubOrderingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maksimkaxxl.foodhuborderingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodHubOrderingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
