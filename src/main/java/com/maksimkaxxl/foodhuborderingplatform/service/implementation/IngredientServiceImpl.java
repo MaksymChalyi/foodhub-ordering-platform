8d6ebd42111d79f63f7f2a5639b3816c3d379c80
Maksym Chalyi <maksymchalyi2004@gmail.com>
2024-03-16 22:46:40 +0200

    Add others files

---
+++ src/main/java/com/maksimkaxxl/foodhuborderingplatform/service/implementation/IngredientServiceImpl.java
@@ -0,0 +1,38 @@
+package com.maksimkaxxl.foodhuborderingplatform.service.implementation;
+
+import com.maksimkaxxl.foodhuborderingplatform.persistense.entity.Ingredient;
+import com.maksimkaxxl.foodhuborderingplatform.persistense.entity.Pizza;
+import com.maksimkaxxl.foodhuborderingplatform.persistense.repository.IngredientRepository;
+import com.maksimkaxxl.foodhuborderingplatform.persistense.repository.PizzaRepository;
+import com.maksimkaxxl.foodhuborderingplatform.service.IngredientService;
+import com.maksimkaxxl.foodhuborderingplatform.service.PizzaService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class IngredientServiceImpl implements IngredientService {
+
+
+    private IngredientRepository ingredientRepository;
+
+
+    @Override
+    public Ingredient findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void create(Ingredient ingredient) {
+        ingredientRepository.save(ingredient);
+    }
+
+    @Override
+    public List<Ingredient> findAll() {
+        return null;
+    }
+}
